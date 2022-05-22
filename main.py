8c8f046a92e076133edb45a2241972feb213eda3
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2022-05-22 12:23:58 +0100

    add load_data function with urllib3
    Co-authored-by: markgreene74 <markgreene74@users.noreply.github.com>
    Co-authored-by: danielalazzara <97309237+danielalazzara@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
 import logging
+import tarfile
+import urllib3
+import os
+
+
+DATA_MAIN_URL = "https://datasets.imdbws.com"
+DATA_FILES = [
+    "name.basics.tsv.gz",
+    "title.akas.tsv.gz",
+    "title.basics.tsv.gz",
+    "title.crew.tsv.gz",
+    "title.episode.tsv.gz",
+    "title.principals.tsv.gz",
+    "title.ratings.tsv.gz",
+]
+
+
+def load_data():
+    logging.info("Loading data")
+
+    for _file in DATA_FILES:
+        _file_url = DATA_MAIN_URL + "/" + _file
+        if os.path.isfile(_file):
+            continue
+        print(f"downloading file {_file_url}")
+
+    """url = 'https://datasets.imdbws.com/name.basics.tsv.gz'
+    chunk_size = 1000
+    http = urllib3.PoolManager()
+    r = http.request('GET', url, preload_content=False)
+
+    with open('text_file', 'wb') as out:
+        while True:
+            data = r.read(chunk_size)
+            if not data:
+                break
+            out.write(data)
+
+    r.release_conn()"""
+
+    """tar = tarfile.open("filename.tar.gz", "r:gz")
+    for member in tar.getmembers():
+        f = tar.extractfile(member)
+        if f is not None:
+            content = f.read()"""
 
 
 def main():
-    logging.info('WHISTAB is starting!')
+    logging.info("WHISTAB is starting!")
+    load_data()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,4 +9,10 @@ LOGGER = logging.getLogger(__name__)
 def test_main(caplog):
     with caplog.at_level(logging.INFO):
         main.main()
-    assert 'WHISTAB is starting!' in caplog.text
+    assert "WHISTAB is starting!" in caplog.text
+
+
+def test_load_data(caplog):
+    with caplog.at_level(logging.INFO):
+        main.load_data()
+    assert "Loading data" in caplog.text
